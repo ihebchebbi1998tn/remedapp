ecf17609a6c43cd61f75d7cf39e901da15844717
Iheb Chebbi <141638922+ihebchebbi1998tn@users.noreply.github.com>
2024-10-31 07:41:15 +0100

    Update WalkViewModel.js

--- Screens/Part1/ViewModels/WalkViewModel.js
+++ Screens/Part1/ViewModels/WalkViewModel.js
@@ -1,16 +1,36 @@
 import { useState } from 'react';
 import * as Location from 'expo-location';
+import { useTranslation } from "react-i18next";
 
 const useWalkViewModel = (navigation) => {
   const [activeDotIndex, setActiveDotIndex] = useState(0);
+  const { t } = useTranslation();
 
-  const handleLocationPermission = async () => {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      alert('Permission to access location was denied');
-      return;
+  const showLocationRequestExplanation = () => {
+    Alert.alert(
+      t('localask'),
+      t('whyweuse'),
+      [
+        { text: t('cancellocali'), style: "cancel" },
+        { text: t('AllowLocal'), onPress: requestLocationPermission }
+      ]
+    );
+  };
+
+  const requestLocationPermission = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      
+      if (status === 'granted') {
+        navigateToLoginScreen();
+      } else {
+        Alert.alert(t('NeededLocal'));
+      }
+      
+    } catch (error) {
+      console.error("Error requesting location permission:", error);
+      Alert.alert('An error occurred while requesting location access. Please try again.');
     }
-    navigation.navigate("LoginScreen");
   };
 
   const navigateToNextScreen = (nextScreen, index) => {
